963ab1e17e07a186cfbd62bc223099c7b764981b
Srikanth-Dell-PC <ksreekanthyadav@gmail.com>
2018-11-27 11:32:13 +0530

    gg

--- app/src/main/java/mahesh/cubex/saloonadmin/MainActivity.java
+++ app/src/main/java/mahesh/cubex/saloonadmin/MainActivity.java
@@ -2,12 +2,73 @@ package mahesh.cubex.saloonadmin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference mDatabaseReference;
+
+    ArrayList<ApprovalRequestPojo> mReList;
+    private NewsListAdapter mAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mReList = new ArrayList<>();
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.re_list);
+
+        mAdapter = new NewsListAdapter(this, mReList);
+
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setAdapter(mAdapter);
+
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference("PaymentRequests");
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mReList.clear();
+                if (dataSnapshot != null) {
+                    for (DataSnapshot dpst : dataSnapshot.getChildren()) {
+                        mReList.add(dpst.getValue(ApprovalRequestPojo.class));
+                    }
+
+                    mAdapter = new NewsListAdapter(MainActivity.this,mReList);
+                    recyclerView.setAdapter(mAdapter);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/mahesh/cubex/saloonadmin/NewsListAdapter.java
@@ -0,0 +1,78 @@
+package mahesh.cubex.saloonadmin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+
+
+public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.MyViewHolder> {
+    private Context context;
+    private List<ApprovalRequestPojo> cartList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, description;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.news_title);
+            description = view.findViewById(R.id.news_description);
+            thumbnail = view.findViewById(R.id.thumbnail);
+        }
+    }
+
+
+    public NewsListAdapter(Context context, List<ApprovalRequestPojo> cartList) {
+        this.context = context;
+        this.cartList = cartList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.news_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        final ApprovalRequestPojo recipe = cartList.get(position);
+        holder.title.setText(recipe.getCustomerType());
+
+
+        Glide.with(context)
+                .load(recipe.getImageUrl())
+                .into(holder.thumbnail);
+
+    }
+
+    // recipe
+    @Override
+    public int getItemCount() {
+        return cartList.size();
+    }
+
+    public static String getDate(long milliSeconds, String dateFormat) {
+        // Create a DateFormatter object for displaying date in specified format.
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
+    }
+}
